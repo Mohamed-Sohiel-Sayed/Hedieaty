9a83e5d13cb57ca717171bdf815f49211bc29534
Mohamed-Sohiel-Sayed <mohamedsohel357@gmail.com>
2024-12-17 20:40:57 +0200

    refine directory structure

--- lib/controllers/pledged_gifts_controller.dart
+++
@@ -1,9 +0,0 @@
-// lib/controllers/pledged_gifts_controller.dart
-import '../models/gift.dart';
-
-class PledgedGiftsController {
-  Future<List<Gift>> getPledgedGifts() async {
-    // Placeholder implementation
-    return [];
-  }
-}
--- lib/services/cloud_service.dart
+++
@@ -1,4 +0,0 @@
-// lib/services/cloud_service.dart
-class CloudService {
-  // Placeholder for any future cloud-related services
-}
