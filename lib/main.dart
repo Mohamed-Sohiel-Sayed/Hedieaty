457ba39889e79a96afee72e1f8d500a72c19d4c7
Mohamed-Sohiel-Sayed <mohamedsohel357@gmail.com>
2024-12-18 03:39:57 +0200

    Use a StreamBuilder to listen to the authentication state

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'routes.dart';
+import 'views/auth/sign_in_page.dart';
+import 'views/home/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(HedieatyApp());
+}
+
+class HedieatyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hedieaty',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AuthWrapper(),
+      onGenerateRoute: AppRoutes.generateRoute,
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<firebase_auth.User?>(
+      stream: firebase_auth.FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return SignInPage();
+        }
+      },
+    );
+  }
+}
